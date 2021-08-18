95cdebb40c0bf42a3f15f2fcf9a264ab9946d6f8
Demilson Pereira <33531233+DemilsonPereira@users.noreply.github.com>
2021-08-17 21:13:09 -0400

    Initial commit

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -0,0 +1,29 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ListAllUsersUseCase {
+  constructor(private usersRepository: IUsersRepository) { }
+
+  execute({ user_id }: IRequest): User[] {
+    const idUser = this.usersRepository.findById(user_id);
+
+    if (!idUser) {
+      throw new Error("Usuário não existe");
+    }
+
+    if (!idUser.admin) {
+      throw new Error("Usuário não é admin");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
+
+  }
+}
+
+export { ListAllUsersUseCase };
